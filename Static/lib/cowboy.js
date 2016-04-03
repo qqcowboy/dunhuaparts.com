5c39efb5234e13a5fddf9dfc0e7c74b344986d70
cowboy <cowboy@localhost.localdomain>
2016-04-03 18:31:53 +0800

    update 20160403

--- Static/lib/cowboy.js
+++ Static/lib/cowboy.js
@@ -43,7 +43,6 @@ ko.bindingHandlers.fadeVisible = {
     init: function (element, valueAccessor) {
         // Initially set the element to be instantly visible/hidden depending on the value
         var value = valueAccessor();
-
         $(element).toggle(ko.utils.unwrapObservable(value));
         // Use "unwrapObservable" so we can handle values that may or may not be observable
     },
@@ -51,6 +50,6 @@ ko.bindingHandlers.fadeVisible = {
     update: function (element, valueAccessor) {
         // Whenever the value subsequently changes, slowly fade the element in or out
         var value = valueAccessor();
-        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).hide();
     }
 };
